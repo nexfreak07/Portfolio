9ccbb7ca0ab8dd1893d8cffea9a4a11714b69c10
Akash Dasgupta <akash.dasgupta@cyberproof.com>
2023-11-17 04:14:29 +0530

    added-script-and-media-queries

---
+++ index.js
@@ -0,0 +1,33 @@
+// Toggle icon navbar
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+  // This is used to create an animation when hamburger is toggled it comes from side
+};
+
+// Active Links
+let section = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top <= offset + height) {
+      // Active navbar links
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
